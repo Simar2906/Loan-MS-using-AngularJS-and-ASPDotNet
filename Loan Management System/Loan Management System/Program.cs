ebd0b9f584240e4cd1b6dd042db7ca640e3c7b9a
Simar2906 <simar2906@gmail.com>
2024-01-31 13:15:48 +0530

    Initialise Html

--- Loan Management System/Loan Management System/Program.cs
+++ Loan Management System/Loan Management System/Program.cs
@@ -4,12 +4,13 @@ namespace Loan_Management_System
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                StartupConfig.ConfigureWebHost(webBuilder);
+            });
     }
 }
---
+++ Loan Management System/Loan Management System/StartupConfig.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace Loan_Management_System
+{
+    public class StartupConfig
+    {
+        public static void ConfigureWebHost(IWebHostBuilder webBuilder)
+        {
+            webBuilder.UseStartup<StartupConfig>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            // Map internal APIs
+            app.Map("/api", apiApp =>
+            {
+                // Configure your internal APIs here
+                // For example: apiApp.UseEndpoints(endpoints => endpoints.MapControllers());
+            });
+
+            // Serve the AngularJS app from the "src" directory
+            app.UseFileServer(new FileServerOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "src")),
+                RequestPath = "",
+                EnableDefaultFiles = true
+            });
+
+            // Set the default route to AngularJS app
+            app.Use(async (context, next) =>
+            {
+                context.Request.Path = "/index.html";
+                await next();
+            });
+
+            // Additional configuration, if needed
+        }
+    }
+}
